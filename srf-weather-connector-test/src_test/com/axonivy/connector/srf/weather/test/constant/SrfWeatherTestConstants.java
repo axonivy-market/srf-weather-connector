4657796c57ad43f3d8df3bbc54b4a1bc29f88f0d
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-30 21:41:50 +0700

    update test case

--- srf-weather-connector-test/src_test/com/axonivy/connector/srf/weather/test/constant/SrfWeatherTestConstants.java
+++ srf-weather-connector-test/src_test/com/axonivy/connector/srf/weather/test/constant/SrfWeatherTestConstants.java
@@ -5,4 +5,6 @@ public class SrfWeatherTestConstants {
 	public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
 	public static final String URL = "url";
 	public static final String TOKEN = "token";
+	public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
+	public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
 }
