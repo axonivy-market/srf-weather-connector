8c6d68963b08e2adf99e0b9a34b68fb002d5d9a3
Dinh Nguyen <127725498+ntqdinh-axonivy@users.noreply.github.com>
2025-08-01 08:43:18 +0700

    MARP-2597 Update test case with new workflow (#46)

--- srf-weather-connector-test/src_test/com/axonivy/connector/srf/weather/test/context/MultiEnvironmentContextProvider.java
+++ srf-weather-connector-test/src_test/com/axonivy/connector/srf/weather/test/context/MultiEnvironmentContextProvider.java
@@ -17,8 +17,12 @@ public class MultiEnvironmentContextProvider implements TestTemplateInvocationCo
 
 	@Override
 	public Stream<TestTemplateInvocationContext> provideTestTemplateInvocationContexts(ExtensionContext context) {
-		return Stream.of(new TestEnironmentInvocationContext(SrfWeatherTestConstants.REAL_CALL_CONTEXT_DISPLAY_NAME),
-				new TestEnironmentInvocationContext(SrfWeatherTestConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
+		String testEnv = System.getProperty(SrfWeatherTestConstants.END_TO_END_TESTING_ENVIRONMENT_KEY);
+		return switch (testEnv) {
+		case SrfWeatherTestConstants.END_TO_END_TESTING_ENVIRONMENT_VALUE ->
+			Stream.of(new TestEnvironmentInvocationContext(SrfWeatherTestConstants.REAL_CALL_CONTEXT_DISPLAY_NAME));
+		default ->
+			Stream.of(new TestEnvironmentInvocationContext(SrfWeatherTestConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
+		};
 	}
-
 }
