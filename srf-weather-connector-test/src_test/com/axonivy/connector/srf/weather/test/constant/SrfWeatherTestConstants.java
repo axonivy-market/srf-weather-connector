1e5cc018293d3e675d82ec3c871eee41913df2ba
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-08-20 13:52:35 +0700

    MARP-2631 Remove unused codes

--- srf-weather-connector-test/src_test/com/axonivy/connector/srf/weather/test/constant/SrfWeatherTestConstants.java
+++ srf-weather-connector-test/src_test/com/axonivy/connector/srf/weather/test/constant/SrfWeatherTestConstants.java
@@ -1,10 +1,6 @@
 package com.axonivy.connector.srf.weather.test.constant;
 
 public class SrfWeatherTestConstants {
-	public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
-	public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
-	public static final String URL = "url";
-	public static final String TOKEN = "token";
-	public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
-	public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
+  public static final String URL = "url";
+  public static final String TOKEN = "token";
 }
