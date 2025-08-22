e085bd7cf6b0da91c0992a3635b8f45228aa1edd
Trần Vĩnh Thiện Phúc <143604440+tvtphuc-axonivy@users.noreply.github.com>
2025-08-22 16:28:09 +0700

    Merge pull request #50 from axonivy-market/feature/MARP-2631-Unify-code-for-e2e-testing-approach

--- srf-weather-connector-test/src_test/com/axonivy/connector/srf/weather/test/TestSrfWeatherForecast.java
+++ srf-weather-connector-test/src_test/com/axonivy/connector/srf/weather/test/TestSrfWeatherForecast.java
@@ -1,16 +1,17 @@
 package com.axonivy.connector.srf.weather.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static com.axonivy.utils.e2etest.enums.E2EEnvironment.REAL_SERVER;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import com.axonivy.connector.srf.weather.test.constant.SrfWeatherTestConstants;
-import com.axonivy.connector.srf.weather.test.context.MultiEnvironmentContextProvider;
-import com.axonivy.connector.srf.weather.test.utils.SrfWeatherTestUtils;
+import com.axonivy.utils.e2etest.context.MultiEnvironmentContextProvider;
+import com.axonivy.utils.e2etest.utils.E2ETestUtils;
 import com.axonivy.connector.srf.weather.connector.SrfWeatherForecastData;
+import com.axonivy.connector.srf.weather.connector.WeatherMock;
 
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
 import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
@@ -23,23 +24,38 @@ import ch.ivyteam.ivy.environment.AppFixture;
 @ExtendWith(MultiEnvironmentContextProvider.class)
 class TestSrfWeatherForecast {
 
-	private static final BpmProcess testee = BpmProcess.path("srfWeatherForecast");
+  private static final BpmProcess testee = BpmProcess.path("srfWeatherForecast");
 
-	@BeforeEach
-	void beforeEach(ExtensionContext context, AppFixture fixture) {
-		SrfWeatherTestUtils.setUpConfigForContext(context.getDisplayName(), fixture);
-	}
+  @BeforeEach
+  void beforeEach(ExtensionContext context, AppFixture fixture) {
+    E2ETestUtils.determineConfigForContext(context.getDisplayName(), runRealEnv(fixture), runMockEnv(fixture));
+  }
 
-	@TestTemplate
-	void locationAndWeatherOfZip(ExtensionContext context, BpmClient bpmClient) throws NoSuchFieldException {
-		BpmElement startable = testee.elementName("call(Number,String)");
-		ExecutionResult result = bpmClient.start().subProcess(startable).execute(6300, "Zug");
-		SrfWeatherForecastData data = result.data().last();
-		if (context.getDisplayName().equals(SrfWeatherTestConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME)) {
-			assertThat(data.getDay().getTNC()).isEqualTo(18);
-		} else {
-			assertThat(data.getLocation().getZip()).isEqualTo(6300);
-		}
-		assertThat(data.getLocation().getLocationName()).isEqualTo("Zug");
-	}
+  @TestTemplate
+  void locationAndWeatherOfZip(ExtensionContext context, BpmClient bpmClient) throws NoSuchFieldException {
+    BpmElement startable = testee.elementName("call(Number,String)");
+    ExecutionResult result = bpmClient.start().subProcess(startable).execute(6300, "Zug");
+    SrfWeatherForecastData data = result.data().last();
+    if (context.getDisplayName().equals(REAL_SERVER.getDisplayName())) {
+      assertThat(data.getDay().getTNC()).isEqualTo(18);
+    } else {
+      assertThat(data.getLocation().getZip()).isEqualTo(6300);
+    }
+    assertThat(data.getLocation().getLocationName()).isEqualTo("Zug");
+  }
+
+  private Runnable runRealEnv(AppFixture fixture) {
+    return () -> {
+      String url = System.getProperty(SrfWeatherTestConstants.URL);
+      String token = System.getProperty(SrfWeatherTestConstants.TOKEN);
+      fixture.var("SrfWeatherConnector.Url", url);
+      fixture.var("SrfWeatherConnector.Token", token);
+    };
+  }
+
+  private Runnable runMockEnv(AppFixture fixture) {
+    return () -> {
+      fixture.config("RestClients.'srfweatherService (SRF Weather)'.Url", WeatherMock.URI);
+    };
+  }
 }
